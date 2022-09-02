321c24dd8da7cc29cfb828689864d3f39f83d0cd
SachinBhattarai0 <sachinbh023@gmail.com>
2022-09-02 22:36:55 +0545

    Edited ReadMe

--- api/db/index.js
+++ api/db/index.js
@@ -4,5 +4,5 @@ require("dotenv").config();
 mongoose
   // .connect("mongodb://127.0.0.1:27017/rms")
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("Connected to db!"))
+  .then(() => console.log("connected to db!"))
   .catch(() => console.log("connection to db failed!"));
